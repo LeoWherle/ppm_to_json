73c60a2305a029d36e519e5f38679755c52bcc2e
Leo Wehrle <leo.wehrle@epitech.eu>
2023-01-15 06:22:19 +0100

    add ssh converter with enough spaces

---
+++ ppm_to_fsh.py
@@ -0,0 +1,55 @@
+def normalize_rgb(rgb_values):
+    return [round(value/255, 3) for value in rgb_values]
+
+def ppm_to_json(ppm_file):
+    try:
+        # Open && read PPM file
+        with open(ppm_file, 'r') as f:
+            contents = f.read().splitlines()
+        if contents[0] != 'P3':
+            raise ValueError("Invalid PPM format, expected 'P3' but got", contents[0])
+        i = 1
+        # Skip comments
+        while contents[i][0] == '#':
+            i += 1
+        width, height = map(int, contents[i].split())
+        max_val = int(contents[i+1])
+        contents = contents[i+2:]
+    except Exception as e:
+        print("An error occured while reading the PPM file: ",e)
+        return
+    # Initialize a list to store the image's pixels
+    image = []
+    row = []
+    color_values = []
+    i = 0
+
+# vec3[4](
+#         vec3(0.0, 0.0, 0.0),
+#         vec3(0.0, 0.0, 0.0),
+#         vec3(0.0, 0.0, 0.0),
+#         vec3(0.0, 0.0, 0.0)
+#     );
+
+    with open("output.fsh", 'w') as f:
+        f.write('vec3[1024] (\n')
+        # Iterate through each pixel's RGB values
+        for value in contents:
+            color_values.append(int(value))
+            # If we have 3 RGB values, we have a pixel
+            if (i+1)%3 == 0:
+                # Normalize the RGB values (can be removed, if you prefer from 0 to 255)
+                #normalized_color_values = normalize_rgb(color_values)
+                # Special use for other project
+                #if sum(normalized_color_values) == 0:
+                #   normalized_color_values[2] = 0.01
+                # end of special use
+                f.write('            vec3({}, {}, {}),\n'.format(*color_values))
+                #row.append(normalized_color_values)
+                color_values = []
+                # If we have a full row, append it to the image
+#                if (i+1)%(3*width) == 0:
+#                    image.append(row)
+#                    row = []
+            i += 1
+        f.write(');')
